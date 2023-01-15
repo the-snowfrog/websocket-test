296a6c032f998183092525efdb32f51c20928a18
Marcel Schmidt <the-snowfrog@users.noreply.github.com>
2023-01-15 17:26:07 +0000

    Websockets implementiert

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+app.UseWebSockets(webSocketOptions);
+
 app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
