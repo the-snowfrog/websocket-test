4685e91ca85f778da9b884a766ecd5bb95cf47c2
Marcel Schmidt <the-snowfrog@users.noreply.github.com>
2023-01-15 15:47:23 +0000

    Publishing entfernt

--- terraform/azure/shared/modules/app/maint.tf
+++ terraform/azure/shared/modules/app/maint.tf
@@ -22,11 +22,3 @@ resource "azurerm_linux_web_app" "app" {
     minimum_tls_version = "1.2"
   }
 }
-
-resource "azurerm_app_service_source_control" "app" {
-  app_id                 = azurerm_linux_web_app.app.id
-  repo_url               = "https://github.com/the-snowfrog/websocket-test"
-  branch                 = "main"
-  use_manual_integration = false
-  use_mercurial          = false
-}
