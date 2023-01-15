f0f4880553eb8af383f864fc44bb15665c9c065b
Marcel Schmidt <the-snowfrog@users.noreply.github.com>
2023-01-15 17:54:53 +0000

    Keine Controller

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+using System.Net.WebSockets;
+using System.Text;
 
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var webSocketOptions = new WebSocketOptions
@@ -8,6 +9,29 @@ var webSocketOptions = new WebSocketOptions
     KeepAliveInterval = TimeSpan.FromMinutes(2)
 };
 app.UseWebSockets(webSocketOptions);
-app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/wss")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            await webSocket.SendAsync(Encoding.UTF8.GetBytes("Hello World!"), WebSocketMessageType.Text, true, CancellationToken.None);
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Bye", CancellationToken.None);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    else
+    {
+        await next(context);
+    }
+
+});
 
 app.Run();
